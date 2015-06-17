d01125227062706ecaf6ef655308d96cca1bbf98
Dan Nemeth <dnemeth@pivotal.io>
2015-06-17 11:28:18 -0400

    Run tests in isolation. Clean up test artifacts.

--- bootswag_test.go
+++ bootswag_test.go
@@ -10,60 +10,73 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var (
+	T1_TARGET string
+	T2_TARGET string
+)
+
 var _ = Describe("Bootswag", func() {
-	var (
-		TEST_TARGET        string
-		TEST_NAME          string
-		TEST_UI_NAME       string
-		TEST_UI_DIR        string
-		TEST_MANIFEST_PATH string
-		TEST_UI_INDEX      string
-	)
-
-	AfterEach(func() {
-		_, err := os.Open(TEST_TARGET)
-		if os.IsNotExist(err) {
-			return
-		} else {
-			if err := os.RemoveAll(TEST_TARGET); err != nil {
-				panic("Could not clean up test artifacts.")
-			}
-		}
-	})
-
 	Describe("default ui at specified location", func() {
-		TEST_TARGET = path.Join(ORGPATH, "/testdir")
-		TEST_NAME = "testname"
-		TEST_UI_NAME = "default"
-		TEST_UI_DIR = path.Join(TEST_TARGET, "/swagger-ui")
-		TEST_MANIFEST_PATH = path.Join(TEST_TARGET, "/manifest.yml")
-		TEST_UI_INDEX = path.Join(TEST_UI_DIR, "/index.html")
+		var (
+			T1_TARGET        = path.Join(ORGPATH, "/test1")
+			T1_NAME          = "testname"
+			T1_UI_NAME       = "default"
+			T1_UI_DIR        = path.Join(T1_TARGET, "/swagger-ui")
+			T1_MANIFEST_PATH = path.Join(T1_TARGET, "/manifest.yml")
+			T1_UI_INDEX      = path.Join(T1_UI_DIR, "/index.html")
+		)
 
 		It("is created successfully", func() {
-			BuildTarget(TEST_TARGET, TEST_NAME, TEST_UI_NAME)
-			Ω(DirectoryExists(TEST_TARGET)).To(BeTrue())
-			Ω(DirectoryExists(TEST_UI_DIR)).To(BeTrue())
-			Ω(fileExists(TEST_MANIFEST_PATH)).To(BeTrue())
-			Ω(matchStringInFile(TEST_MANIFEST_PATH, TEST_NAME)).To(BeTrue())
-			Ω(matchStringInFile(TEST_UI_INDEX, "Pez")).To(BeFalse())
+			BuildTarget(T1_TARGET, T1_NAME, T1_UI_NAME)
+			Ω(DirectoryExists(T1_TARGET)).To(BeTrue())
+			Ω(DirectoryExists(T1_UI_DIR)).To(BeTrue())
+			Ω(fileExists(T1_MANIFEST_PATH)).To(BeTrue())
+			Ω(matchStringInFile(T1_MANIFEST_PATH, T1_NAME)).To(BeTrue())
+			Ω(matchStringInFile(T1_UI_INDEX, "pez")).To(BeFalse())
 		})
 	})
 
 	Describe("pez ui at specified location", func() {
-		TEST_TARGET = path.Join(ORGPATH, "/testdir")
-		TEST_NAME = "peztest"
-		TEST_UI_NAME = "pez"
-		TEST_UI_DIR = path.Join(TEST_TARGET, "/swagger-ui")
-		TEST_MANIFEST_PATH = path.Join(TEST_TARGET, "/manifest.yml")
-		TEST_UI_INDEX = path.Join(TEST_UI_DIR, "/index.html")
+		var (
+			T2_TARGET        = path.Join(ORGPATH, "/test2")
+			T2_NAME          = "peztest"
+			T2_UI_NAME       = "pez"
+			T2_UI_DIR        = path.Join(T2_TARGET, "/swagger-ui")
+			T2_MANIFEST_PATH = path.Join(T2_TARGET, "/manifest.yml")
+			T2_UI_INDEX      = path.Join(T2_UI_DIR, "/index.html")
+		)
 
 		It("is created successfully", func() {
-			BuildTarget(TEST_TARGET, TEST_NAME, TEST_UI_NAME)
-			Ω(DirectoryExists(TEST_TARGET)).To(BeTrue())
-			Ω(DirectoryExists(TEST_UI_DIR)).To(BeTrue())
-			Ω(fileExists(TEST_MANIFEST_PATH)).To(BeTrue())
-			Ω(matchStringInFile(TEST_MANIFEST_PATH, TEST_NAME)).To(BeTrue())
-			Ω(matchStringInFile(TEST_UI_INDEX, "pez")).To(BeTrue())
+			BuildTarget(T2_TARGET, T2_NAME, T2_UI_NAME)
+			Ω(DirectoryExists(T2_TARGET)).To(BeTrue())
+			Ω(DirectoryExists(T2_UI_DIR)).To(BeTrue())
+			Ω(fileExists(T2_MANIFEST_PATH)).To(BeTrue())
+			Ω(matchStringInFile(T2_MANIFEST_PATH, T2_NAME)).To(BeTrue())
+			Ω(matchStringInFile(T2_UI_INDEX, "pez")).To(BeTrue())
 		})
 	})
 })
+
+var _ = AfterSuite(func() {
+	cleanUp()
+})
+
+func cleanUp() {
+	var (
+		t1 = path.Join(ORGPATH, "/test1")
+		t2 = path.Join(ORGPATH, "/test2")
+	)
+	_, err := os.Open(t1)
+	if !os.IsNotExist(err) {
+		if err := os.RemoveAll(t1); err != nil {
+			panic("Could not clean up test artifacts.")
+		}
+	}
+
+	_, err = os.Open(t2)
+	if !os.IsNotExist(err) {
+		if err := os.RemoveAll(t2); err != nil {
+			panic("Could not clean up test artifacts.")
+		}
+	}
+}
